b762cbf14f9899e5d29cb536bada935d66395ace
xanax <34755589+anKordii@users.noreply.github.com>
2022-08-07 17:56:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import {stream_info, send_tweet} from "./modules/index.js";
 import {isOnline} from "./components/index.js";
 dotenv.config()
 
-const app = express;
+const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
@@ -28,6 +28,33 @@ let yfl = {
     xkaleson: false,
     adrian1g__: false
 }
+
+app.get('/', function (req, res) {
+    res.send(`
+    <!DOCTYPE html>
+    <html lang="en"> 
+        <head> 
+            <meta charset="utf-8" /> 
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" /> 
+            <title>YFLUPDATES SOCKET</title> 
+            <meta property="og:type" content="website" /> 
+            <link rel="icon" type="image/png" sizes="560x560" href="https://cdn.beyondlabs.pl/assets/img/logo512.png" /> 
+            <style> 
+                @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap'); 
+                body{ margin: 0; padding: 0; font-family: 'Roboto', sans-serif; } 
+                .bl__box{ height: 100%; width: 100%; position: absolute; text-align: left; display: grid; place-content: center; } 
+                h1{ font-weight: 100; color: black !important; } 
+                a{ font-weight: 100; color: #565ac6; text-decoration: none; } 
+            </style> 
+        </head> 
+        <body class="bl__box"> 
+            <div> 
+                <h1>YFL UPDATES SOCKET</h1> 
+                <h2>Created by <a href="https://seven7s.top/">seven7s.top</a> 
+            </div> 
+        </body>
+    </html>`)
+})
 
 io.on('connection', (socket) => {
     //console.log('a user connected');
