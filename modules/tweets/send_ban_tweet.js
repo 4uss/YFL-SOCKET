894b0aa75c1d1460331d5bbb16f280fecf466190
xanax <34755589+anKordii@users.noreply.github.com>
2022-08-16 20:56:45 +0200

    Update send_ban_tweet.js

--- modules/tweets/send_ban_tweet.js
+++ modules/tweets/send_ban_tweet.js
@@ -4,6 +4,7 @@ async function send_ban_tweet(i){
 
     twit_bans.post('statuses/update', { status: `${i.banned.toUpperCase()} został zbanowany na kanale ${i.channel}
 
+https://yfl.seven7s.top/user/${i.banned}
 #YFL #TWITCH #YFLBANS` }, function(err, data, response) {
     })
 }
